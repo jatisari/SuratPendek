c2380764d6f91c08da445904cc9409d26cfcead2
jatisari <jatisari.innovation@gmail.com>
2018-04-08 20:15:52 +0700

    Initial commit

---
+++ app/src/main/java/net/agusharyanto/suratpendek/Surat.java
@@ -0,0 +1,46 @@
+package net.agusharyanto.suratpendek;
+
+/**
+ * Created by Agus Haryanto on 09/03/2018.
+ */
+
+public class Surat {
+
+    private int mId; //id
+    private String name; // nama surat
+    private String filePath; //nama file
+
+    public Surat()
+    {
+    }
+
+    public Surat(int id, String name, String filepath)
+    {
+        setName(name);
+        setFilePath(filepath);
+    }
+
+    public int getmId() {
+        return mId;
+    }
+
+    public void setmId(int mId) {
+        this.mId = mId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+}
